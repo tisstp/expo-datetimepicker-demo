86f2c62c47c82920a58ec07ed825039cea8fa703
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-02-23 20:32:34 +0700

    demo datetime picker

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Platform, StyleSheet, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [local, setLocal] = useState('en-US');
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+
+    setDate(currentDate);
+    setShow(Platform.OS === 'ios');
+  };
+
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const changeLocal = () => {
+    setLocal(local === 'en-US' ? 'th-TH' : 'en-US');
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Button onPress={changeLocal} title="Change local"/>
+      </View>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!"/>
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!"/>
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          locale={local}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    top: 100,
+    // flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,34 @@
+# Expo DateTimePicker Demo
+
+
+## Environment 
+
+- Expo CLI Version: `3.13.1`
+- Node Version: `v12.14.1`
+- NPM Version: `6.13.6`
+- Yarn Version: `1.21.1`
+
+
+## Dependencies
+
+- [date-time-picker](https://docs.expo.io/versions/latest/sdk/date-time-picker/)
+
+
+## Install
+`expo install @react-native-community/datetimepicker`
+
+
+## Run Application
+
+```shell script
+# run ios
+yarn run ios
+
+# run android
+yarn run android
+```
+
+
+## Demo
+
+![ios-datetimepicker](assets/ios-datetimepicker.png)
